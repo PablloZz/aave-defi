2445e94f952604e7231b5a18434ac73e48fec195
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-06-28 20:05:40 +0300

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,48 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { type HardhatUserConfig } from "hardhat/types";
+import { vars } from "hardhat/config";
+
+const SEPOLIA_RPC_URL = vars.get("SEPOLIA_RPC_URL");
+const MAINNET_RPC_URL = vars.get("MAINNET_RPC_URL");
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const COINMARKETCAP_API_KEY = vars.get("COINMARKETCAP_API_KEY");
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
+      allowUnlimitedContractSize: true,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  solidity: {
+    compilers: [{ version: "0.8.24" }, { version: "0.8.19" }, { version: "0.4.19" }],
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  mocha: {
+    timeout: 500000,
+  },
+};
+
+export default config;
---
+++ scripts/getWeth.ts
@@ -0,0 +1,21 @@
+import { type Contract } from "ethers";
+import { ethers } from "hardhat";
+import { type IWeth } from "../typechain-types";
+
+const VALUE_TO_DEPOSIT = ethers.parseEther("0.02");
+
+async function getWeth() {
+  const deployer = (await ethers.getSigners())[0];
+  const iWeth = (await ethers.getContractAt(
+    "IWeth",
+    "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    deployer,
+  )) as Contract & IWeth;
+
+  const transactionResponse = await iWeth.deposit({ value: VALUE_TO_DEPOSIT });
+  transactionResponse.wait(1);
+  const wethBalance = await iWeth.balanceOf(deployer);
+  console.log(`Got ${ethers.formatEther(wethBalance)}`);
+}
+
+export { getWeth };
